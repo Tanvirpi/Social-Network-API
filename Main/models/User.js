378d3f3e0c1165a53237dbb7a730cd06bfbdf711
tanvirpi <tanvirpi@gmail.com>
2023-05-19 14:26:50 -0600

    user js updated

---
+++ Main/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,'enter valid email']
+    },
+
+    createDate: {
+      type: Date,
+      // Sets a default value of 12 weeks from now
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ], 
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
